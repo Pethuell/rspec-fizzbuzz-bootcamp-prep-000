51c81ab41f7e6d84ee9d979f627dfa4b34c629be
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-09-04 10:14:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  end
+  elsif num % 3 == 0
+    "Fizz"
+  end
+  elsif num % 5 == 0
+    "Buzz"
+  end
+else
+end
 end
